cd31dda225419c254d23ab96688d1c4a9d521604
Jacob Winch <jacob.winch@guardian.co.uk>
2023-12-20 14:18:39 +0000

    Enable nightly backups for DynamoDB table

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,12 @@
 import "source-map-support/register";
-import { App } from "aws-cdk-lib";
-import { GoogleSearchIndexChecker } from "../lib/google-search-index-checker";
 import {GuRootExperimental} from "@guardian/cdk/lib/experimental/constructs/root";
+import type { App } from "aws-cdk-lib";
+import { GoogleSearchIndexChecker } from "../lib/google-search-index-checker";
 
 const app: App = new GuRootExperimental();
-new GoogleSearchIndexChecker(app, "GoogleSearchIndexChecker-PROD", { stack: "ophan", stage: "PROD", env: {region: "eu-west-1"} });
+new GoogleSearchIndexChecker(app, "GoogleSearchIndexChecker-PROD", {
+    stack: "ophan",
+    stage: "PROD",
+    env: { region: "eu-west-1" },
+    withBackup: true
+});
