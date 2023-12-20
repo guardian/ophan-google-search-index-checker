3199e631034614a4db8a99307c50c6b988c6fb60
Jacob Winch <jacob.winch@guardian.co.uk>
2023-12-20 14:30:46 +0000

    GuRoot is no longer experimental

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,9 +1,9 @@
 import "source-map-support/register";
-import {GuRootExperimental} from "@guardian/cdk/lib/experimental/constructs/root";
+import { GuRoot } from "@guardian/cdk/lib/constructs/root";
 import type { App } from "aws-cdk-lib";
 import { GoogleSearchIndexChecker } from "../lib/google-search-index-checker";
 
-const app: App = new GuRootExperimental();
+const app: App = new GuRoot();
 new GoogleSearchIndexChecker(app, "GoogleSearchIndexChecker-PROD", {
     stack: "ophan",
     stage: "PROD",
